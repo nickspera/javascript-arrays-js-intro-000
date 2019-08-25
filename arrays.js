d45f0c3c12ae1d60fbcb625824b34665cd1a042f
Nick Spera <nickaspera@gmail.com>
2019-08-25 00:40:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,10 +31,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 
-// function removeElementFromBeginningOfArray(array){
-//   array.slice(0);
-//   return array;
-// }
+function removeElementFromBeginningOfArray(array){
+  array.slice(-2);
+  return array;
+}
 
 function removeElementFromEndOfArray(array) {
   array.splice(-1);
